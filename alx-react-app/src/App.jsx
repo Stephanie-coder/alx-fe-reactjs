3526a5804197bdd4ec74260e18961d671b6a47b3
Stephanie <stephaniesheryl@gmail.com>
2025-08-31 21:37:51 +0300

    Task 2: Added Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,17 @@
-import './App.css'
-import WelcomeMessage from "./components/WelcomeMessage";
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <WelcomeMessage />
-    </div>
-  )
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
+
