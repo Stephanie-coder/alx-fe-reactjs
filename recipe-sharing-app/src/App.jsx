818212ed16252ffe86378e0476675caeabb54731
Stephanie <stephaniesheryl@gmail.com>
2025-09-16 00:43:48 +0300

    update app.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
@@ -10,21 +10,23 @@ import RecommendationsList from './components/RecommendationsList';
 
 const App = () => {
   return (
-    <div style={{ padding: '1rem' }}>
-      <h1>Recipe Sharing App</h1>
+    <Router>
+      <div style={{ padding: '1rem' }}>
+        <h1>Recipe Sharing App</h1>
 
-      <AddRecipeForm />
-      <SearchBar />
+        <AddRecipeForm />
+        <SearchBar />
 
-      <Routes>
-        <Route path="/" element={<RecipeList />} />
-        <Route path="/recipes/:id" element={<RecipeDetails />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+        </Routes>
 
-      {/* ✅ Add the new components here */}
-      <FavoritesList />
-      <RecommendationsList />
-    </div>
+        {/* ✅ Add the new components here */}
+        <FavoritesList />
+        <RecommendationsList />
+      </div>
+    </Router>
   );
 };
 
