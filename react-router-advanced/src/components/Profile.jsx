4a056a5bdd8d35030d8abf268bf32eaf5db4e07c
Stephanie <stephaniesheryl@gmail.com>
2025-10-12 15:54:45 +0300

    Fix nested routes in Profile.jsx with ProfileDetails and ProfileSettings

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,14 +1,28 @@
-import { Outlet, Link } from 'react-router-dom'
+import { Routes, Route, Link } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 export default function Profile() {
   return (
     <div>
-      <h1>Profile</h1>
+      <h1>Profile Page</h1>
+
       <nav>
-        <Link to="details">Profile Details</Link> |{' '}
-        <Link to="settings">Profile Settings</Link>
+        <ul>
+          <li>
+            <Link to="details">Profile Details</Link>
+          </li>
+          <li>
+            <Link to="settings">Profile Settings</Link>
+          </li>
+        </ul>
       </nav>
-      <Outlet />
+
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
     </div>
-  )
+  );
 }
+
